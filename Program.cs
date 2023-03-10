6d4ab3b9c9ddf36e8086e6d4a471370605e546b9
Connor Davis <connor@auto-bot.co.za>
2023-03-10 10:51:46 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleRecipes
+{
+    internal class Program
+    {
+        /**
+         * The Main method will execute all of the functions
+         * required to run this application.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // Just print hello world to the console.
+        }
+    }
+}
